af020e64af3b570d75c9bf302561f4f1f9869109
Moritz Welsch <moritz@welsch.pro>
2025-02-02 09:24:39 +0100

    Adding interface

---
+++ src/FileProcessor/FileProcessor.java
@@ -0,0 +1,14 @@
+package FileProcessor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public interface FileProcessor {
+    /**
+     * Reads Data from a file and returns the extracted data as array
+     * @param filePath the absolute file path of which to read
+     * @return An Array with the values of the
+     */
+    default ArrayList read(String filePath){return null;}
+    default Arrays write(String filePath){return null;}
+}
