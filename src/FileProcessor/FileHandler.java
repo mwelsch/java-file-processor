a21f6195822d66d5d5b9b1e418e04fc666c329a0
Moritz Welsch <moritz@welsch.pro>
2025-02-02 09:36:43 +0100

    Constructing basic concept

---
+++ src/FileProcessor/FileHandler.java
@@ -0,0 +1,22 @@
+package FileProcessor;
+
+public class FileHandler {
+    /**
+     * Reads the file from the specified path and returns its content as string
+     * @param path the absoulte path to the file to be read
+     * @return The file content as string
+     */
+    public static String filePathToString(String path){
+
+        return "Hallo!";
+    }
+
+    /**
+     * Writes a string to a file
+     * @param path absolute path of the output file
+     * @param data the string which should be written to the file
+     */
+    public static void writeDataToFilePath(String path, String data){
+        
+    }
+}
--- src/FileProcessor/JSONFileProcessor.java
+++ src/FileProcessor/JSONFileProcessor.java
@@ -3,18 +3,29 @@ package FileProcessor;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public interface FileProcessor {
+public class JSONFileProcessor implements FileProcessor{
+
     /**
      * Reads Data from a file and returns the extracted data as array
+     *
      * @param filePath the absolute file path of which to read
-     * @return An Array with the values of the File
+     * @return An Array with the values of the
      */
-    public ArrayList<Object> read(String filePath);
+    @Override
+    public ArrayList<Object> read(String filePath) {
+        return null;
+    }
 
     /**
      * Writes data from an Array to a specified path in the Format of the implemented FileProcessor
+     *
      * @param filePath where the data should be written to
-     * @param data What is contained in the data
+     * @param data     What is contained in the data
      */
-    public void write(String filePath, Arrays data);
+    @Override
+    public void write(String filePath, Arrays data) {
+
+    }
+
+
 }
